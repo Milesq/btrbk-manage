79328390771f0e74a4cc5a4c7ebf8f096164f6fe
Milesq <milesq@proton.me>
2025-10-01 23:35:35 +0200

    feat(protect): check error on exit

--- cmd/btrbk-protect/main.go
+++ cmd/btrbk-protect/main.go
@@ -11,11 +11,17 @@ import (
 
 func main() {
 	p := tea.NewProgram(protect.InitialModel("./mnt/@snaps"), tea.WithAltScreen())
+
 	m, err := p.Run()
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
 
+	mm := m.(protect.Model)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error:", mm.Err)
+		os.Exit(1)
 	}
 }
