94864dec5360c7a1f59cd81c4f33bf06b0410192
Milesq <milesq@proton.me>
2025-10-02 23:14:57 +0200

    feat: add availableSubvolumes and utilities to BackupManager

--- internal/snaps/manager.go
+++ internal/snaps/manager.go
@@ -1,9 +1,36 @@
 package snaps
 
+import (
+	"fmt"
+	"slices"
+)
+
 func GetManagerForDirectory(snapDirectory string) BackupManager {
-	return BackupManager{snapDirectory}
+	return BackupManager{snapDirectory, nil, nil}
 }
 
 type BackupManager struct {
-	dir string
+	dir                 string
+	availableSubvolumes []string
+	subvolumes          []string
+}
+
+func (mng *BackupManager) AvailableSubvolumes() []string {
+	result := make([]string, len(mng.availableSubvolumes))
+	copy(result, mng.availableSubvolumes)
+	return result
+}
+
+func (mng *BackupManager) SetSubvolumes(subvolumes []string) error {
+	for _, subvol := range subvolumes {
+		if !mng.isAvailable(subvol) {
+			return fmt.Errorf("subvolume %q is not available", subvol)
+		}
+	}
+	mng.subvolumes = subvolumes
+	return nil
+}
+
+func (mng *BackupManager) isAvailable(subvolume string) bool {
+	return slices.Contains(mng.availableSubvolumes, subvolume)
 }
