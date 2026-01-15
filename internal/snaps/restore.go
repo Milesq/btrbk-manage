3ae1500ed08d32faaff6bd7fa3970d1107c55c28
Milesq <milesq@proton.me>
2026-01-15 23:43:52 +0100

    feat(restore/hooks): execute post-restore hook

--- internal/snaps/restore.go
+++ internal/snaps/restore.go
@@ -1,8 +1,10 @@
 package snaps
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"time"
 
@@ -14,6 +16,29 @@ func (mng *BackupManager) Restore(backup Backup, subvolumes []string) error {
 		if err := mng.addRestorationDate(backup.Timestamp); err != nil {
 			return fmt.Errorf("failed to update restoration date: %w", err)
 		}
+	}
+
+	if err := mng.executeHook("post-restore"); err != nil {
+		return fmt.Errorf("post-restore hook failed: %w", err)
+	}
+
+	return nil
+}
+
+func (mng *BackupManager) executeHook(name string) error {
+	hookPath := filepath.Join(mng.hooksDir, name+".sh")
+
+	if _, err := os.Stat(hookPath); os.IsNotExist(err) {
+		return nil
+	}
+
+	var stderr bytes.Buffer
+	cmd := exec.Command(hookPath)
+	cmd.Stderr = &stderr
+	cmd.Env = append(os.Environ(), fmt.Sprintf("RESTORE_PATH=%s", mng.dir))
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to execute %s: %w, stderr: %s", hookPath, err, stderr.String())
 	}
 
 	return nil
